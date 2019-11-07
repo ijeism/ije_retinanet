d0a2b0989dc62f5916b7618ee0533bc5a62b36c2
ijeism <onyeji.i@gmail.com>
2019-11-07 16:10:46 +0000

    Delete create directories.py

--- create directories.py
+++
@@ -1,9 +0,0 @@
-# create directories
-os.mkdir("POSITIVES/")
-os.mkdir("NEGATIVES/")
-os.mkdir("training/")
-os.mkdir("training/POSITIVES/")
-os.mkdir("training/NEGATIVES/")
-os.mkdir("testing/")
-os.mkdir("testing/POSITIVES/")
-os.mkdir("testing/NEGATIVES/")
